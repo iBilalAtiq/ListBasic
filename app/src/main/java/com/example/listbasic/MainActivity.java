020da221b388f8096b4b6abafdd26c6729560229
iBilalAtiq <iBilalAtiq@users.noreply.github.com>
2020-07-23 15:04:27 +0500

    List Basic

--- app/src/main/java/com/example/listbasic/MainActivity.java
+++ app/src/main/java/com/example/listbasic/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.listbasic;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.ListBasic);
+        final ArrayList<String> arrayList = new ArrayList<String>();
+        arrayList.add("Bilal");
+        arrayList.add("Atiq");
+        arrayList.add("SHireen");
+        arrayList.add("Zainab");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "Hello " + arrayList.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
